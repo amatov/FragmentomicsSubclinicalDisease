530e314d4179161f3b962f1937b8d08a5dddad0e
Alex  Matov <matov@clin.au.dk>
2020-12-25 10:45:37 +0000

    Replace kld_crc_test.py

--- kld_crc_test.py
+++ kld_crc_test.py
@@ -9,7 +9,9 @@ from est_rel_entro_HJW import *
 #sampP = pd.read_csv('delfi1_h364_23310.csv')
 #sampP = pd.read_csv('delfi1_crc.csv')
 #sampP = pd.read_csv('delfi1_ctl198.csv')
-sampP = pd.read_csv('delfi2_ctl3_71.csv') # consistent with Delfi1 analysis
+#sampP = pd.read_csv('delfi2_ctl3_71.csv') # of 131
+#sampP = pd.read_csv('delfi2_ctl2_71.csv')
+sampP = pd.read_csv('delfi2_ctl1_74.csv')
 
 #sampQ = pd.read_csv('delfi1_crc198.csv')
 #sampQ = pd.read_csv('delfi1_lcc.csv')
@@ -20,12 +22,19 @@ sampP = pd.read_csv('delfi2_ctl3_71.csv') # consistent with Delfi1 analysis
 #sampQ = pd.read_csv('delfi1_ovc.csv')
 #sampQ = pd.read_csv('delfi1_c364_14430.csv')
 #sampQ = pd.read_csv('delfi1_ctl.csv') # to test the reverse divergence
-sampQ = pd.read_csv('delfi2_col_all79.csv')
+#sampQ = pd.read_csv('delfi2_rec_all50.csv')
+#sampQ = pd.read_csv('delfi2_col1_all8.csv')
+sampQ = pd.read_csv('delfi2_col2_all30.csv')
+
 
 est = est_rel_entro_HJW(sampP, sampQ)
 print(est)
 estP = pd.DataFrame(est)
-estP.to_csv('KLdivergenceD2_COL79_ctl3_71.csv')
+estP.to_csv('KLdivergenceD2_COL2_30_ctl1.csv')
+#estP.to_csv('KLdivergenceD2_COL1_8_ctl1.csv')
+#estP.to_csv('KLdivergenceD2_REC50_ctl1.csv')
+#estP.to_csv('KLdivergenceD2_REC50_ctl2.csv')
+#estP.to_csv('KLdivergenceD2_REC50_ctl3.csv')
 #estP.to_csv('KLdivergenceCRC_Reverse.csv')
 #estP.to_csv('KLdivergenceCRC364.csv')
 #estP.to_csv('KLdivergenceCRC.csv')
