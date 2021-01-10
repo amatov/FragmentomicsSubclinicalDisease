5ff52fd55d4719d3cf3585b7c7f25a2d5e098803
Alex  Matov <matov@clin.au.dk>
2021-01-10 10:50:50 +0000

    Replace kld_crc_test.py

--- kld_crc_test.py
+++ kld_crc_test.py
@@ -7,7 +7,9 @@ from est_rel_entro_HJW import *
 
 #sampP = pd.read_csv('delfi1_crc364.csv')
 #sampP = pd.read_csv('delfi1_h364_23310.csv')
-#sampP = pd.read_csv('delfi1_crc.csv')
+sampP = pd.read_csv('delfi1_ctl.csv')
+#sampP = pd.read_csv('delfi1_ctl1Bins.csv')
+#sampP = pd.read_csv('delfi2_ctl1Bins.csv')
 #sampP = pd.read_csv('delfi1_ctl198.csv')
 #sampP = pd.read_csv('delfi2_ctl3_71.csv') # of 131
 #sampP = pd.read_csv('delfi2_ctl2_71.csv')
@@ -16,8 +18,10 @@ from est_rel_entro_HJW import *
 #sampP = pd.read_csv('delfi2_ctl1_fr365.csv')
 #sampP = pd.read_csv('delfi1_ctl43_frl499.csv')
 #sampP = pd.read_csv('delfi1_ctl43_frl499_189.csv')
-sampP = pd.read_csv('delfi1_healthy43_555bins_fr189.csv')
-
+#sampP = pd.read_csv('delfi1_healthy43_555bins_fr189.csv')
+#sampP = pd.read_csv('delfi2_ctl1_fr137.csv')
+#sampP = pd.read_csv('delfi2_ctl1_fr205.csv')
+#sampP = pd.read_csv('delfi2_ctl1_fr364.csv')
 
 #sampQ = pd.read_csv('delfi1_crc198.csv')
 #sampQ = pd.read_csv('delfi1_lcc.csv')
@@ -36,12 +40,46 @@ sampP = pd.read_csv('delfi1_healthy43_555bins_fr189.csv')
 #sampQ = pd.read_csv('delfi2_col_fr195.csv')
 #sampQ = pd.read_csv('delfi2_col_fr365.csv')
 #sampQ = pd.read_csv('delfi2_col79_bin555_189.csv')
-sampQ = pd.read_csv('delfi2_val210_555bins_fr189.csv')
+#sampQ = pd.read_csv('delfi2_val210_555bins_fr189.csv')
+
+#list = [17, 18, 19]
+#print(list)
+#for i in list: #range(13,79):
+#file_name = 'delfi2_col_adeH20.csv'
+#kld_name = 'KLdivergenceD2col_adeH20_ctl1.csv'
+#file_name = 'delfi2_col_adeL28.csv'
+#kld_name = 'KLdivergenceD2col_adeL28_ctl1.csv'
+#file_name = 'delfi2_rec_adeH11.csv'
+#kld_name = 'KLdivergenceD2rec_adeH11_ctl1.csv'
+#file_name = 'delfi2_rec_adeL7.csv'
+#kld_name = 'KLdivergenceD2rec_adeL7_ctl1.csv'
+#file_name = 'delfi2_col1_fr205.csv'
+#kld_name = 'KLdivergenceD2col1_fr205_ctl1.csv'
+#file_name = 'delfi2_col2_fr137.csv'
+#kld_name = 'KLdivergenceD2col2_fr137_ctl1.csv'
+#file_name = 'delfi2_col3_fr364.csv'
+#kld_name = 'KLdivergenceD2col3_fr364_ctl1.csv'
+#file_name = 'delfi2_col4_fr364.csv'
+#kld_name = 'KLdivergenceD2col4_fr364_ctl1.csv'
+#file_name = 'delfi1_crcBins.csv'
+#kld_name = 'KLdivergenceCRC_Bins.csv'
+#file_name = 'delfi2_colBins.csv'
+#kld_name = 'KLdivergenceD2col_Bins.csv'
+#file_name = 'delfi2_ctl1_individual%d.csv'%(i)
+#kld_name = 'KLdivergenceD2ctl1_individual%d.csv'%(i)
+i=3
+#for i in range(12, 43, 1):
+file_name = 'delfi1_ctl1_individual%d.csv'%(i)
+kld_name = 'KLdivergenceD1crc1_individual%d.csv'%(i)
+#for i in range(1,79):
+sampQ = pd.read_csv(file_name)
+#sampQ = pandas.read_csv('delfi2_col_individual{0}.csv'.format(i))
 
 est = est_rel_entro_HJW(sampP, sampQ)
 print(est)
 estP = pd.DataFrame(est)
-estP.to_csv('KLdivergenceD2val210_D1ctl43_fr189.csv')
+estP.to_csv(kld_name)
+#estP.to_csv('KLdivergenceD2val210_D1ctl43_fr189.csv')
 #estP.to_csv('KLdivergenceD2col79_D1ctl43_fr189.csv')
 #estP.to_csv('KLdivergenceD2col79_D1ctl43.csv')
 #estP.to_csv('KLdivergenceD2_COL4_23_ctl2.csv')
