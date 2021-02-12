8d56f32cb380e561c1e60b3ce62bc4677bf65cba
Alex  Matov <matov@clin.au.dk>
2021-02-12 12:51:30 +0000

    Replace mnist_convet.py

--- mnist_convet.py
+++ mnist_convet.py
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env python
-
+import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -11,7 +11,7 @@ import pandas as pd
 """
 
 # Model / data parameters
-num_classes = 2#10
+num_classes = 2#4#2#10
 #input_shape = (28,28,1)
 input_shape = (574, 499, 1)
 
@@ -23,29 +23,41 @@ input_shape = (574, 499, 1)
 #print("y_test shape:", y_test.shape)
 
 # read four XLS files: SamplesTr, SelectionTr, SamplesTe, SelectionTe
-x_train = pd.read_csv('~/genomedk/DELFI2/Workspaces/matov/MNIST/samplesTr.csv') # 69+64 x 574 x 499
-y_train = pd.read_csv('~/genomedk/DELFI2/Workspaces/matov/MNIST/selectionTr.csv') # vector 69 1s and 64 0s
-x_test = pd.read_csv('~/genomedk/DELFI2/Workspaces/matov/MNIST/samplesTe.csv') # 10+10 x 574 x 499
-y_test = pd.read_csv('~/genomedk/DELFI2/Workspaces/matov/MNIST/selectionTe.csv') # vector 10 1s and 10 0s
-#x_train = pd.read_csv('~/matovanalysis/DELFI_analysis/python/samplesTr.csv') # 69+64 x 574 x 499
-#y_train = pd.read_csv('~/matovanalysis/DELFI_analysis/python/selectionTr.csv') # vector 69 1s and 64 0s
-#x_test = pd.read_csv('~/matovanalysis/DELFI_analysis/python/samplesTe.csv') # 10+10 x 574 x 499
-#y_test = pd.read_csv('~/matovanalysis/DELFI_analysis/python/selectionTe.csv') # vector 10 1s and 10 0s
+x_train = pd.read_csv('~/matovanalysis/DELFI_analysis/python/samplesTr.csv') # 69+64 x 574 x 499
+y_train = pd.read_csv('~/matovanalysis/DELFI_analysis/python/selectionTr.csv') # vector 69 1s and 64 0s
+x_test = pd.read_csv('~/matovanalysis/DELFI_analysis/python/samplesTe.csv') # 10+10 x 574 x 499
+y_test = pd.read_csv('~/matovanalysis/DELFI_analysis/python/selectionTe.csv') # vector 10 1s and 10 0s
+#x_train = pd.read_csv('~/matovanalysis/DELFI_analysis/python/samplesTr4.csv') # 69+10+18+64 x 574 x 499
+#y_train = pd.read_csv('~/matovanalysis/DELFI_analysis/python/selectionTr4.csv') # vector 69 4s,10 3s, 18 2s, 64 1s
+#x_test = pd.read_csv('~/matovanalysis/DELFI_analysis/python/samplesTe4.csv') # 10+10+10+10 x 574 x 499
+#y_test = pd.read_csv('~/matovanalysis/DELFI_analysis/python/selectionTe4.csv') # vector 10 4s, 10 3s, 10 2s, 10 1s
 print("x_train shape:", x_train.shape)
 print("y_train shape:", y_train.shape)
 print("x_test shape:", x_test.shape)
 print("y_test shape:", y_test.shape)
+# these should be of size 161 by 574 by 499, not 161 by 574*499+1
+
 # Scale images to the [0, 1] range
 #x_train = x_train.astype("float32") / 255
 #x_test = x_test.astype("float32") / 255
+
 # Make sure images have shape (28, 28, 1)
 #x_train = np.expand_dims(x_train, -1)
 #x_test = np.expand_dims(x_test, -1)
-#print("x_train shape:", x_train.shape)
-#print(x_train.shape[0], "train samples")
-#print(x_test.shape[0], "test samples")
+x_train = x_train.iloc[:,1:(574*499+1)]
+x_test = x_test.iloc[:,1:(574*499+1)]
+print("x_train1 shape:", x_train.shape)
+print("x_test1 shape:", x_test.shape)
 
+x_train = tf.reshape(x_train, [133, 574, 499])#[161, 574, 499])
+x_test = tf.reshape(x_test, [20, 574, 499])#[40, 574 , 499])
 
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
 # convert class vectors to binary class matrices
 #y_train = keras.utils.to_categorical(y_train, num_classes)
 #y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -73,8 +85,8 @@ model.summary()
 ## Train the model
 """
 
-batch_size = 128
-epochs = 15
+batch_size = 32#128
+epochs = 1#2#15
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
@@ -87,3 +99,4 @@ model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_spl
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
+print(score)
