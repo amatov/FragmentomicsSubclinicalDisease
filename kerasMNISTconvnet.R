3ee9c6ef7aeda1a3f47ebda4cc114c0cd5dec74c
Alex  Matov <matov@clin.au.dk>
2021-02-12 12:52:36 +0000

    Upload New File

---
+++ kerasMNISTconvnet.R
@@ -0,0 +1,40 @@
+
+# Model / data parameters
+num_classes = 4#2#10
+#input_shape = (28,28,1)
+input_shape = (574, 499, 1)
+
+
+model = keras.Sequential(
+  [
+    keras.Input(shape=input_shape),
+    layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Flatten(),
+    layers.Dropout(0.5),
+    layers.Dense(num_classes, activation="softmax"),
+  ]
+)
+
+model.summary()
+
+"""
+## Train the model
+"""
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+"""
+## Evaluate the trained model
+"""
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
